bebb2cfbf44a6477ec38bdf0566bc0cf722fbd4c
Joey Binas <joseph.t.binas@gmail.com>
2022-08-18 13:04:32 -0700

    refactored App.js to include apolloserver/graphql

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from "@apollo/client/link/context";
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// create our main GraphQL API endpoint
+// ApolloClient uses HttpLink by default when we provide uri option to ApolloClient constructor
+// can be used for GET and POST requests
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// setContext modifies the context object and returns a new one
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  // concatenate the authLink middleware to GraphQL API endpoint (need to ask how this works in OH)
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
+
     <Router>
       <>
         <Navbar />
@@ -25,6 +57,8 @@ function App() {
         </Routes>
       </>
     </Router>
+
+    </ApolloProvider>
   );
 }
 
