8690c242ecd23fb1323be2b7bcd6d5aa23460a27
Joey Binas <joseph.t.binas@gmail.com>
2022-08-18 13:03:49 -0700

    added apolloserver refactor

--- server/server.js
+++ server/server.js
@@ -3,8 +3,22 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// adding dependencies to utilize Apollo Server
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+// middleware
+const {authMiddleware} = require("./utils/auth");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// establish new Apollo server with defined params and context
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,6 +30,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+// initialize ApolloServer here
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    })
+  })
+};
+
+startApolloServer(typeDefs, resolvers)
